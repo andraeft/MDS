9819dc17fbb078c39ce1391bf974e71fa9a7f3be
lucian.bc <lucianboaca97@gmail.com>
2018-04-13 13:34:21 +0300

    Initial commit

---
+++ backend/payit/payit-app/src/main/java/com/payit/app/App.java
@@ -0,0 +1,19 @@
+package com.payit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping(value = {"/", "/hello"})
+    public String greet() {
+        return "Hello, World";
+    }
+}
---
+++ backend/payit/payit-app/src/test/java/com/payit/app/AppTest.java
@@ -0,0 +1,17 @@
+package com.payit.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AppTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}

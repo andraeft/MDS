b022d5f4c356a22d00aa03c18581bede6ab1ed3b
lucian.bc <lucianboaca97@gmail.com>
2018-04-22 23:33:14 +0300

    Configured security on the back end.

--- backend/payit/payit-app/src/main/java/com/payit/app/App.java
+++ backend/payit/payit-app/src/main/java/com/payit/app/App.java
@@ -1,7 +1,9 @@
 package com.payit.app;
 
+import com.payit.security.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,23 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@Import({
+        SecurityConfig.class
+})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @GetMapping(value = {"/", "/hello"})
+    @GetMapping(value = {"/api", "/api/hello"})
     public Object greet() {
         Map<String, String> resp = new HashMap<>();
         resp.put("message", UUID.randomUUID().toString());
         return resp;
+    }
+
+    @GetMapping(value = "/public")
+    public String publicMethod() {
+        return "{\"message\": \"Hello, public\"}";
     }
 }
---
+++ backend/payit/payit-security/src/main/java/com/payit/security/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.payit.security.config;
+
+import com.payit.security.MyAuthenticationSuccessHandler;
+import com.payit.security.RestAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.payit.security")
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+
+    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+    private final MyAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public SecurityConfig(RestAuthenticationEntryPoint authenticationEntryPoint,
+                          MyAuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/api", "/api/**").authenticated()
+                    .and()
+                .formLogin()
+                    .permitAll()
+                    .loginProcessingUrl("/login")
+                    .successHandler(authenticationSuccessHandler)
+                    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                    .and()
+                .logout()
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll();
+    }
+
+    @Override
+    @Autowired
+    @SuppressWarnings({"squid:CallToDeprecatedMethod", "deprecation"})
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("user")
+                .password("password")
+                .roles("USER");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}

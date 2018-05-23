64df2b47c6b01b017c1f585581c839346ab48a62
Byzanteen <andreiopreacretu@yahoo.fr>
2018-05-23 12:15:01 +0300

    modify user+test

--- backend/payit/payit-app/src/main/java/com/payit/app/service/UserService.java
+++ backend/payit/payit-app/src/main/java/com/payit/app/service/UserService.java
@@ -5,15 +5,34 @@ import com.payit.app.repository.UserRepository;
 import com.payit.security.model.Credentials;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService{
     private final UserRepository repo;
 
-    public void createUser(Credentials credential) {
+    public User createUser(Credentials credential) {
         User newUser = new User();
         newUser.setFkCredentials(credential);
-        repo.save(newUser);
+        return repo.save(newUser);
+
+    }
+
+    @Transactional
+    public void modifyUser(String email,String firstName,String lastName,Integer id){
+        User modifyUser=repo.findById(id).orElse(null);
+        if(modifyUser==null)
+            return;
+        if(email!=null)
+            modifyUser.setEmail(email);
+        if(firstName!=null)
+            modifyUser.setFirstName(firstName);
+        if(lastName!=null)
+            modifyUser.setLastName(lastName);
+    }
+
+    public User findById(Integer id) {
+        return repo.findById(id).orElse(null);
     }
 }

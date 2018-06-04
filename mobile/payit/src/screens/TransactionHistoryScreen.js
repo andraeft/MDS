eb025bd2a420f4d71847448c3eb87e777641b61f
andra eft <andraeftene@gmail.com>
2018-06-04 14:29:39 +0300

    updated screens

--- mobile/payit/src/screens/TransactionHistoryScreen.js
+++ mobile/payit/src/screens/TransactionHistoryScreen.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Card, Button, FormLabel, FormInput } from 'react-native-elements';
+
+export default () => (
+    <View>
+        chat
+    </View>
+);

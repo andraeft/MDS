d6d62cd4beaa7f8f2050c72950f058b00a525e59
andra eft <andraeftene@gmail.com>
2018-06-04 19:01:31 +0300

    transaction history - afisare buton + 2 liste

--- mobile/payit/src/screens/TransactionHistoryScreen.js
+++ mobile/payit/src/screens/TransactionHistoryScreen.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Card, Button, FormLabel, FormInput } from 'react-native-elements';
+import React, {Component} from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { Card, Button, FormLabel, FormInput, List, ListItem } from 'react-native-elements';
 
-export default () => (
-    <View>
-        chat
+
+
+const Componenta = ({lista}) => (
+    <View style={{ paddingVertical: 20 }}>
+        <Button buttonStyle={{ marginTop: 20 }} background-color="#03A9F4" title="new transaction"
+        />
+    <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, display: 'flex', flexDirection: 'row', borderColor: 'pink' }}>
+        <FlatList 
+        data={[{key: 'de primit'}, {key: 'de dat'}]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+        />
+    </List>
+
+    <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList 
+        data={lista}
+        renderItem={({listItem}) => (
+            <Text>{JSON.stringify(lista)}</Text> 
+          )}
+        />
+    </List> 
     </View>
 );
+
+Componenta.defaultProps = {
+    lista: [
+        {   key: 1,
+            donator: 'andrei',
+            primitor: 'anca',
+            suma: 200
+        },
+        {   key: 2,
+            donator: 'julieta',
+            primitor: 'romeo',
+            suma: 400
+        }
+    ]
+}
+
+export default Componenta;
